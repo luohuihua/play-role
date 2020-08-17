163e6825648e380a6cbf0de4a7dba929e406737b
luohuihua <295228341@qq.com>
2020-08-17 18:06:55 +0800

    TASK 0 增加开启事务注解

--- playrole-server/src/main/java/com/luohh/playrole/PlayroleApplication.java
+++ playrole-server/src/main/java/com/luohh/playrole/PlayroleApplication.java
@@ -3,12 +3,14 @@ package com.luohh.playrole;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 启动类
  */
 @SpringBootApplication
 @MapperScan("com.luohh.playrole.*.mapper")
+@EnableTransactionManagement
 public class PlayroleApplication {
 
     public static void main(String[] args) {
